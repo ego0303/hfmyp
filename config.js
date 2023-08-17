cccc6de044fdd35380929ce1e747c51f0e14b1d3
FouIII <97218937+FouIII@users.noreply.github.com>
2023-08-18 01:01:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tố Uyên",
+    titleWeb: "Ánh Minh",
 
-    background: "IMG_1695.jpg",
+    background: "IMG_1695.jpeg",
 
-    min1: "IMG_1653.jpg",
-    min2: "IMG_1688.jpg",
-    min3: "IMG_1689.jpg",
-    min4: "img4.jpg",
-    min5: "img5.jpg",
-    min6: "img6.jpg",
+    min1: "IMG_1653.jpeg",
+    min2: "IMG_1688.jpeg",
+    min3: "IMG_1689.jpeg",
+    min4: "IMG_1690.jpeg",
+    min5: "IMG_1691.jpeg",
+    min6: "IMG_1694.jpeg",
 
-    max1: "IMG_1694.jpeg",
-    max2: "IMG_1695.jpeg",
-    max3: "IMG_1696.jpg",
-    max4: "IMG_1697.jpg",
-    max5: "IMG_1698.jpg",
-    max6: "IMG_1700.jpg",
+    max1: "IMG_1695.jpeg",
+    max2: "IMG_1696.jpeg",
+    max3: "IMG_1697.jpeg",
+    max4: "IMG_1698.jpeg",
+    max5: "IMG_1698.jpeg",
+    max6: "IMG_1700.jpeg",
 }
