345852b6f66fac31caf1a8fca0e2bb31cac9e75b
FouIII <97218937+FouIII@users.noreply.github.com>
2024-01-17 21:26:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "Uyn Uyn",
+    titleWeb: "Jaaeeny",
 
-    background: "img8.jpg",
+    background: "background.jpg",
 
-    min1: "img1.jpg",
-    min2: "img2.jpg",
-    min3: "img3.jpg",
-    min4: "img4.jpg",
-    min5: "img5.jpg",
-    min6: "img6.jpg",
+    min1: "12.jpg",
+    min2: "2.jpg",
+    min3: "3.jpg",
+    min4: "4.jpg",
+    min5: "5.jpg",
+    min6: "6.jpg",
 
-    max1: "img7.jpg",
-    max2: "img8.jpg",
-    max3: "img9.jpg",
-    max4: "img10.jpg",
-    max5: "img3.jpg",
-    max6: "img8.jpg",
+    max1: "7.jpg",
+    max2: "8.jpg",
+    max3: "9.jpg",
+    max4: "10.jpg",
+    max5: "11.jpg",
+    max6: "1.jpg",
 }
