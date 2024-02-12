17711f637e5a350c76935da4a6e711d9624ed367
nniini <97218937+FouIII@users.noreply.github.com>
2024-02-13 02:21:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "Khánh Linh",
+    titleWeb: "Thu Hiền",
 
-    background: "1.jfif",
+    background: "1.jpg",
 
-    min1: "1.jfif",
-    min2: "2.jfif",
-    min3: "3.jfif",
-    min4: "4.jfif",
-    min5: "5.jfif",
-    min6: "6.jfif",
+    min1: "1.jpg",
+    min2: "2.jpg",
+    min3: "3.jpg",
+    min4: "4.jpg",
+    min5: "5.jpg",
+    min6: "6.jpg",
 
-    max1: "7.jfif",
-    max2: "8.jfif",
-    max3: "9.jfif",
-    max4: "10.jfif",
-    max5: "3.jfif",
-    max6: "1.jfif",
+    max1: "1.jpg",
+    max2: "2.jpg",
+    max3: "3.jpg",
+    max4: "4.jpg",
+    max5: "5.jpg",
+    max6: "6.jpg",
 }
