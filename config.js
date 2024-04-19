f9ad784b2eecbfccffa20887c13c93228bd88b9e
ego <97218937+ego0303@users.noreply.github.com>
2024-04-20 00:23:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "Minh Ngọc",
+    titleWeb: "Khánh Chi",
 
-    background: "3.jpg",
+    background: "2.jpg",
 
-    min1: "1.jfif",
-    min2: "2.jfif",
+    min1: "1.jpg",
+    min2: "2.jpg",
     min3: "3.jpg",
-    min4: "4.jfif",
-    min5: "5.jfif",
-    min6: "6.jfif",
+    min4: "4.jpg",
+    min5: "5.jpg",
+    min6: "6.jpg",
 
-    max1: "7.jfif",
-    max2: "1.jfif",
-    max3: "2.jfif",
-    max4: "3.jpg",
-    max5: "4.jfif",
-    max6: "5.jfif",
+    max1: "7.jpg",
+    max2: "8.jpg",
+    max3: "9.jpg",
+    max4: "10.jpg",
+    max5: "11.jpg",
+    max6: "12.jpg",
 }
